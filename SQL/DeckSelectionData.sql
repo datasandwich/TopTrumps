b81806fd2d76a24aace2c20a02a98ae99dcfe6cb
Royabe <104497648+Royabe@users.noreply.github.com>
2022-06-22 16:55:51 +0100

    DeckSelectionData added

---
+++ SQL/DeckSelectionData.sql
@@ -0,0 +1,6 @@
+﻿insert into DeckSelection
+values (0,'Magic the Gathering Planeswalkers','\TopTrumps\wwwroot\images\MTGDeck\BackImage.jpg');
+insert into DeckSelection
+values (1,'Animals','\TopTrumps\wwwroot\images\Animals\BackImage.png');--Magda, replace this with the path of the image you use for the back
+insert into DeckSelection
+values (2,'Greek Mythology','\TopTrumps\wwwroot\images\GreekMyth\BackImage.jpg');--Jarek, replace this with the path of the image you use for the back
