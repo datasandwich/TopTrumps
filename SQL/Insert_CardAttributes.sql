840c03bfa6ff070a9957d03775b569fdfae6858c
datasandwich <jarekettl98@gmail.com>
2022-06-22 17:56:10 +0200

    Added to 'insert attribute' sql query

---
+++ SQL/Insert_CardAttributes.sql
@@ -0,0 +1,4 @@
+INSERT INTO CardAttributes(Id,DeckId,attrname1,attrname2,attrname3,attrname4,attrname5)
+values (0,0,'Year Since First Appearance','Number of Cards','Highest Mana Cost','Strength','Intelligence');
+INSERT INTO CardAttributes(Id,DeckId,attrname1,attrname2,attrname3,attrname4,attrname5)
+values (2,2,'Bravery','Wisdom','Strength','Ferocity','Fear Factor');
