64a5c7cec5d5f28d0da5ad75b5fdfc72ed099ade
Royabe <104497648+Royabe@users.noreply.github.com>
2022-06-23 12:26:09 +0100

    More deck stuff

--- SQL/MTGData.sql
+++ SQL/MTGData.sql
@@ -1,40 +1,40 @@
+insert into Cards(DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
+values (0,'Gideon', 'Hero at Akros, Leader of the gatewatch, and ally of Zendikar',12,8,6,10,2,'\TopTrumps\wwwroot\images\MTGdeck\Gideon.jpg')
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (0,0,'Gideon', 'Hero at Akros, Leader of the gatewatch, and ally of Zendikar',12,8,6,10,2,'\TopTrumps\wwwroot\images\MTGdeck\Gideon.jpg')
+values (0,'Jace', 'Mind mage scholar of Vryn, Investigated anomalies on innistrad and lost himself on ixalan',15,12,6,1,10,'\TopTrumps\wwwroot\images\MTGdeck\Jace.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (1,0,'Jace', 'Mind mage scholar of Vryn, Investigated anomalies on innistrad and lost himself on ixalan',15,12,6,1,10,'\TopTrumps\wwwroot\images\MTGdeck\Jace.jpg');
+values (0,'Chandra','Daughter of outlaw artificers on kaladesh, fueled by rage at their authorities',15,17,6,7,3,'\TopTrumps\wwwroot\images\MTGdeck\Chandra.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (2,0,'Chandra','Daughter of outlaw artificers on kaladesh, fueled by rage at their authorities',15,17,6,7,3,'\TopTrumps\wwwroot\images\MTGdeck\Chandra.jpg');
+values (0,'Nissa','Elf of zendikar, attuned to the land and all its inhabitants',13,11,7,6,5,'\TopTrumps\wwwroot\images\MTGdeck\Nissa.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (3,0,'Nissa','Elf of zendikar, attuned to the land and all its inhabitants',13,11,7,6,5,'\TopTrumps\wwwroot\images\MTGdeck\Nissa.jpg');
+values (0,'Liliana','Ascendant necromancer who made pacts for power with several demons, killing them all',15,12,7,8,7,'\TopTrumps\wwwroot\images\MTGdeck\Liliana.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (4,0,'Liliana','Ascendant necromancer who made pacts for power with several demons, killing them all',15,12,7,8,7,'\TopTrumps\wwwroot\images\MTGdeck\Liliana.jpg');
+values (0,'Ajani','Part of the nacatl pride on alara, valiant protector who joined the gatewatch',15,12,6,9,2,'\TopTrumps\wwwroot\images\MTGdeck\Ajani.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (5,0,'Ajani','Part of the nacatl pride on alara, valiant protector who joined the gatewatch',15,12,6,9,2,'\TopTrumps\wwwroot\images\MTGdeck\Ajani.jpg');
+values (0,'Nicol Bolas','Elder dragon seeking omnipotence and omniscience to ascend to godhood',28,6,8,12,12,'\TopTrumps\wwwroot\images\TopTrumps\wwwroot\images\MTGdeck\Bolas.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (6,0,'Nicol Bolas','Elder dragon seeking omnipotence and omniscience to ascend to godhood',28,6,8,12,12,'\TopTrumps\wwwroot\images\TopTrumps\wwwroot\images\MTGdeck\Bolas.jpg');
+values (0,'Ugin', 'Spirit elder dragon, brother and rival to Bolas, Helped stop bolas ascending',7,2,8,8,12,'\TopTrumps\wwwroot\images\MTGdeck\Ugin.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (7,0,'Ugin', 'Spirit elder dragon, brother and rival to Bolas, Helped stop bolas ascending',7,2,8,8,12,'\TopTrumps\wwwroot\images\MTGdeck\Ugin.jpg');
+values (0,'Tezzeret','Master artificer from alara. Although human, he is more machine than man',14,8,6,6,8,'\TopTrumps\wwwroot\images\MTGdeck\Tezzeret.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (8,0,'Tezzeret','Master artificer from alara. Although human, he is more machine than man',14,8,6,6,8,'\TopTrumps\wwwroot\images\MTGdeck\Tezzeret.jpg');
+values (0,'Teferi','Temporal mage from the isle of zalphir, which he sent out of time for its protection',16,8,6,4,10,'\TopTrumps\wwwroot\images\MTGdeck\Teferi.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (9,0,'Teferi','Temporal mage from the isle of zalphir, which he sent out of time for its protection',16,8,6,4,10,'\TopTrumps\wwwroot\images\MTGdeck\Teferi.jpg');
+values (0,'Karn','Sentient silver golem created by the nigh-omniscient artificer Urza',24,4,7,8,7,'\TopTrumps\wwwroot\images\MTGdeck\Karn.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (10,0,'Karn','Sentient silver golem created by the nigh-omniscient artificer Urza',24,4,7,8,7,'\TopTrumps\wwwroot\images\MTGdeck\Karn.jpg');
+values (0,'Ob Nixilis','Demon of Zendikar who uses death magic to crush the weak',13,6,6,7,2,'\TopTrumps\wwwroot\images\MTGdeck\Nixilis.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (11,0,'Ob Nixilis','Demon of Zendikar who uses death magic to crush the weak',13,6,6,7,2,'\TopTrumps\wwwroot\images\MTGdeck\Nixilis.jpg');
+values (0,'Kaya','Corporeal and incorporeal assassin of the orzhov, targets both living and dead',6,5,6,9,4,'\TopTrumps\wwwroot\images\MTGdeck\Kaya.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (12,0,'Kaya','Corporeal and incorporeal assassin of the orzhov, targets both living and dead',6,5,6,9,4,'\TopTrumps\wwwroot\images\MTGdeck\Kaya.jpg');
+values (0,'Tamiyo', 'Chronicler and moon sage of kamigawa, remade by phyrexians on her return',10,4,5,3,8,'\TopTrumps\wwwroot\images\MTGdeck\Tamiyo.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (13,0,'Tamiyo', 'Chronicler and moon sage of kamigawa, remade by phyrexians on her return',10,4,5,3,8,'\TopTrumps\wwwroot\images\MTGdeck\Tamiyo.jpg');
+values (0,'Tibalt','Devilish trickster on innistrad who feigned death to return as the god of trickery on kaldheim',10,4,7,3,7,'\TopTrumps\wwwroot\images\MTGdeck\Tibalt.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (14,0,'Tibalt','Devilish trickster on innistrad who feigned death to return as the god of trickery on kaldheim',10,4,7,3,7,'\TopTrumps\wwwroot\images\MTGdeck\Tibalt.jpg');
+values (0,'Narset','Enlightened master of the jeskai of tarkir, combines both martial arts and magic',8,4,6,7,7,'\TopTrumps\wwwroot\images\MTGdeck\Narset.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (15,0,'Narset','Enlightened master of the jeskai of tarkir, combines both martial arts and magic',8,4,6,7,7,'\TopTrumps\wwwroot\images\MTGdeck\Narset.jpg');
+values (0,'Arlinn','Werewolf of Innistrad who helped the humans fight against the beings of the night',6,3,6,9,5,'\TopTrumps\wwwroot\images\MTGdeck\Arlinn.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (16,0,'Arlinn','Werewolf of Innistrad who helped the humans fight against the beings of the night',6,3,6,9,5,'\TopTrumps\wwwroot\images\MTGdeck\Arlinn.jpg');
+values (0,'Ral Zarek','Izzet guildmaster on ravnica. Master of experimental electronics and devices (and lightning)',9,4,6,5,9,'\TopTrumps\wwwroot\images\MTGdeck\Ral.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (17,0,'Ral Zarek','Izzet guildmaster on ravnica. Master of experimental electronics and devices (and lightning)',9,4,6,5,9,'\TopTrumps\wwwroot\images\MTGdeck\Ral.jpg');
+values (0,'Dovin Baan','Vedalken beaurocratic lawyer on kaladesh, enforces strict order and control over artifice',5,4,6,1,8,'\TopTrumps\wwwroot\images\MTGdeck\Dovin.jpg');
 insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (18,0,'Dovin Baan','Vedalken beaurocratic lawyer on kaladesh, enforces strict order and control over artifice',5,4,6,1,8,'\TopTrumps\wwwroot\images\MTGdeck\Dovin.jpg');
-insert into Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (19,0,'Ashiok','The embodiment of nightmares on theros, steals dreams and weaves nightmares in their place',9,4,6,3,6,'\TopTrumps\wwwroot\images\MTGdeck\Ashiok.jpg');
+values (0,'Ashiok','The embodiment of nightmares on theros, steals dreams and weaves nightmares in their place',9,4,6,3,6,'\TopTrumps\wwwroot\images\MTGdeck\Ashiok.jpg');
---
+++ TopTrumps/Models/Deck.cs
@@ -0,0 +1,60 @@
+﻿namespace TopTrumps.Models;
+using System.Data.SqlClient;
+
+public class Deck
+{
+    private Queue<Card>? _cards { get; set; }
+    private string? _owner { get; set; }
+    public Deck(Queue<Card>? cards, string? owner)
+    {
+        _cards = cards;
+        _owner = owner;
+    }
+    public string? getOwner() { return _owner; }
+    public Queue<Card>? getCards() { return _cards; }
+
+    public void getShuffled()
+    {
+        if (_cards != null)
+        {
+            Queue<Card> shuffled = new Queue<Card>();
+
+            shuffled = Shuffle();
+            foreach (var card in _cards)
+            {
+                _cards.Enqueue(shuffled.Dequeue());
+                _cards.Dequeue();
+            }
+        }
+    }
+    private Queue<Card> Shuffle()
+    {
+        if (_cards != null) {
+            List<Card> cards = new List<Card>();
+            Queue<Card> shuffled = new Queue<Card>();
+            foreach (var card in _cards)
+            {
+                cards.Add(card);
+            }
+            int index = 0;
+            foreach (var card in _cards)
+            {
+                Random Jump = new Random();
+                index += Jump.Next(cards.Count);
+                shuffled.Enqueue(cards[index]);
+                cards.RemoveAt(index);
+            }
+            return shuffled;
+        }
+        return new Queue<Card>();
+    }
+    public async void Load(int deckNo)
+    {
+        Queue<Card> cards = new Queue<Card>();
+        for (int i = 0; i < 20; i++)
+        {
+
+        }
+        
+    }
+}
