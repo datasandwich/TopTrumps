e1f47fb71b3f3c28fa89d4c0304533c295a17aa2
datasandwich <jarekettl98@gmail.com>
2022-06-23 11:24:43 +0200

    Edited SQL Query

--- SQL/Insert_Jareks_Cards.sql
+++ SQL/Insert_Jareks_Cards.sql
@@ -1,40 +1,40 @@
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (0,2,'Minotaur','',12,25,138,18,40,'\TopTrumps\wwwroot\images\GM\Minotaur.jpeg');
+values (42,2,'Minotaur','',12,25,138,18,40,'\TopTrumps\wwwroot\images\GM\Minotaur.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (1,2,'Odysseus','',30,56,65,10,11,'\TopTrumps\wwwroot\images\GM\Oddysseus.webp');
+values (43,2,'Odysseus','',30,56,65,10,11,'\TopTrumps\wwwroot\images\GM\Oddysseus.webp');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (2,2,'Heracles','',15,55,170,13,22,'\TopTrumps\wwwroot\images\GM\Heracles.jpeg');
+values (44,2,'Heracles','',15,55,170,13,22,'\TopTrumps\wwwroot\images\GM\Heracles.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (3,2,'Pegasus','',20,20,180,9,37,'\TopTrumps\wwwroot\images\GM\Pegasus.webp');
+values (45,2,'Pegasus','',20,20,180,9,37,'\TopTrumps\wwwroot\images\GM\Pegasus.webp');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (4,2,'Polyphemus','',8,18,140,15,47,'\TopTrumps\wwwroot\images\GM\Polyphemus.jpeg');
+values (46,2,'Polyphemus','',8,18,140,15,47,'\TopTrumps\wwwroot\images\GM\Polyphemus.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (5,2,'Icarus','',18,65,50,2,9,'\TopTrumps\wwwroot\images\GM\Icarus.jpeg');
+values (47,2,'Icarus','',18,65,50,2,9,'\TopTrumps\wwwroot\images\GM\Icarus.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (6,2,'Graeae','',6,99,30,1,40,'\TopTrumps\wwwroot\images\GM\Graeae.jpeg');
+values (48,2,'Graeae','',6,99,30,1,40,'\TopTrumps\wwwroot\images\GM\Graeae.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (7,2,'Chimera','',5,10,140,20,48,'\TopTrumps\wwwroot\images\GM\Chimera.jpeg');
+values (49,2,'Chimera','',5,10,140,20,48,'\TopTrumps\wwwroot\images\GM\Chimera.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (8,2,'Zeus','',10,100,200,8,40,'\TopTrumps\wwwroot\images\GM\Zeus.jpeg');
+values (50,2,'Zeus','',10,100,200,8,40,'\TopTrumps\wwwroot\images\GM\Zeus.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (9,2,'Hermes','',18,84,100,6,29,'\TopTrumps\wwwroot\images\GM\Hermes.webp');
+values (51,2,'Hermes','',18,84,100,6,29,'\TopTrumps\wwwroot\images\GM\Hermes.webp');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (10,2,'Medusa','',9,25,50,6,50,'\TopTrumps\wwwroot\images\GM\Medusa.jpeg');
+values (52,2,'Medusa','',9,25,50,6,50,'\TopTrumps\wwwroot\images\GM\Medusa.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (11,2,'Athena','',20,100,190,8,35,'\TopTrumps\wwwroot\images\GM\Athena.jpeg');
+values (53,2,'Athena','',20,100,190,8,35,'\TopTrumps\wwwroot\images\GM\Athena.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (12,2,'Poseidon','',12,90,190,8,30,'\TopTrumps\wwwroot\images\GM\Poseidon.jpeg');
+values (54,2,'Poseidon','',12,90,190,8,30,'\TopTrumps\wwwroot\images\GM\Poseidon.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (13,2,'Bellerophon','',25,40,145,5,5,'\TopTrumps\wwwroot\images\GM\Bellerophon.jpeg');
+values (55,2,'Bellerophon','',25,40,145,5,5,'\TopTrumps\wwwroot\images\GM\Bellerophon.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (14,2,'Perseus','',25,45,75,5,10,'\TopTrumps\wwwroot\images\GM\Perseus.jpeg');
+values (56,2,'Perseus','',25,45,75,5,10,'\TopTrumps\wwwroot\images\GM\Perseus.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (15,2,'Kratos','',25,40,190,8,40,'\TopTrumps\wwwroot\images\GM\Kratos.webp');
+values (57,2,'Kratos','',25,40,190,8,40,'\TopTrumps\wwwroot\images\GM\Kratos.webp');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (16,2,'Aphrodite','',5,25,5,5,5,'\TopTrumps\wwwroot\images\GM\Aphrodite.jpeg');
+values (58,2,'Aphrodite','',5,25,5,5,5,'\TopTrumps\wwwroot\images\GM\Aphrodite.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (17,2,'Gera','',10,30,10,5,5,'\TopTrumps\wwwroot\images\GM\Gera.jpeg');
+values (59,2,'Gera','',10,30,10,5,5,'\TopTrumps\wwwroot\images\GM\Gera.jpeg');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (18,2,'Hephaestus','',20,40,80,10,10,'\TopTrumps\wwwroot\images\GM\Hephaestus.png');
+values (60,2,'Hephaestus','',20,40,80,10,10,'\TopTrumps\wwwroot\images\GM\Hephaestus.png');
 INSERT INTO Cards(id,DeckId,[Name],Flavourtext,attr1,attr2,attr3,attr4,attr5,Imagepath)
-values (19,2,'Dionysus','',5,100,5,5,5,'\TopTrumps\wwwroot\images\GM\Dionysus.jpeg');
+values (61,2,'Dionysus','',5,100,5,5,5,'\TopTrumps\wwwroot\images\GM\Dionysus.jpeg');
