ce21a7638ee9cad2abd907c872e558a5459055c9
Magda <magdagozdalik28@gmail.com>
2022-06-24 08:36:12 +0100

    Updated resources, sql query and Menu

--- SQL/SQLQuery2.sql
+++ SQL/SQLQuery2.sql
@@ -1,42 +1,42 @@
 ﻿insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Dung Beetle',1000,5,3,2,1, 'They can pull 1,141 times their own body weight.','\images\animals\dungBeetle.png');
+values (1,'Dung Beetle',1000,5,3,2,1, 'They can pull 1,141 times their own body weight.','\images\animals\dungBeetle.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Eagle',100, 320, 20, 4, 10, 'Able to lift something four times its own body weight during flight','\images\animals\eagle.png');
+values (1,'Eagle',100, 320, 20, 4, 10, 'Able to lift something four times its own body weight during flight','\images\animals\eagle.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Anaconda', 400,14,0,50,2, 'Lives in Africa','\images\animals\anaconda.png');
+values (1, 'Anaconda', 400,14,0,50,2, 'Lives in Africa','\images\animals\anaconda.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Blue Whale', 1000, 50, 60, 2, 27, 'Endangered','\images\animals\bluewhale.png');
+values (1,'Blue Whale', 1000, 50, 60, 2, 27, 'Endangered','\images\animals\bluewhale.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Grizzly Bear', 500, 56, 55, 100, 23 , 'Able to lift something four times its own body weight during flight','\images\animals\brownBrear.png');
+values (1, 'Grizzly Bear', 500, 56, 55, 100, 23 , 'Able to lift something four times its own body weight during flight','\images\animals\brownBrear.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Bull',350, 40, 19, 95, 7,'Lorem ipsum ','\images\animals\bull.png');
+values (1, 'Bull',350, 40, 19, 95, 7,'Lorem ipsum ','\images\animals\bull.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Cheetah',125, 120, 57, 80,33, 'Lorem ipsum ','\images\animals\cheetah.png');
+values (1, 'Cheetah',125, 120, 57, 80,33, 'Lorem ipsum ','\images\animals\cheetah.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Koala',3, 10, 100, 2,26, 'Lorem ipsum ','\images\animals\koala.png');
+values (1,'Koala',3, 10, 100, 2,26, 'Lorem ipsum ','\images\animals\koala.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Crocodile',800, 11, 0, 66,32, 'Lorem ipsum ','\images\anim als\crocodile.png');
+values (1, 'Crocodile',800, 11, 0, 66,32, 'Lorem ipsum ','\images\anim als\crocodile.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Dolphin',25, 12, 100, 44,31, 'Lorem ipsum ','\images\animals\dolphin.png');
+values (1,'Dolphin',25, 12, 100, 44,31, 'Lorem ipsum ','\images\animals\dolphin.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Elephant',999, 40, 65, 58,13, 'Elephants can carry a staggering seven tons.','\images\animals\Chimpanzee.png');
+values (1, 'Elephant',999, 40, 65, 58,13, 'Elephants can carry a staggering seven tons.','\images\animals\Chimpanzee.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Horse',75, 88, 75, 34,9, 'Lorem ipsum ','\images\animals\Chimpanzee.png');
+values (1,'Horse',75, 88, 75, 34,9, 'Lorem ipsum ','\images\animals\Chimpanzee.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Chimpanzee',120, 85, 8, 25,42, 'Lorem ipsum ','\images\animals\Chimpanzee.png');
+values (1, 'Chimpanzee',120, 85, 8, 25,42, 'Lorem ipsum ','\images\animals\Chimpanzee.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Kitten',1, 1, 100, 1,21, 'Lorem ipsum ','\images\animals\Chimpanzee.png');
+values (1, 'Kitten',1, 1, 100, 1,21, 'Lorem ipsum ','\images\animals\Chimpanzee.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Lion',200, 80, 43, 86,8, 'xx','\images\animals\lion.png');
+values (1, 'Lion',200, 80, 43, 86,8, 'xx','\images\animals\lion.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Lizard',1, 28, 5, 2,2, 'Lorem ipsum ','\images\animals\lizard.png');
+values (1, 'Lizard',1, 28, 5, 2,2, 'Lorem ipsum ','\images\animals\lizard.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Ostrich',9, 70, 34, 24,5, 'xx','\images\animals\ostrich.png');
+values (1, 'Ostrich',9, 70, 34, 24,5, 'xx','\images\animals\ostrich.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Red Fox',13, 56, 44, 15,7, 'Lorem ipsum ','\images\animals\redFox.png');
+values (1, 'Red Fox',13, 56, 44, 15,7, 'Lorem ipsum ','\images\animals\redFox.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Rhino',400, 55, 11, 56,28, 'Lorem ipsum ','\images\animals\rhino.png');
+values (1,'Rhino',400, 55, 11, 56,28, 'Lorem ipsum ','\images\animals\rhino.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1, 'Squirel',3, 20, 87, 9,9, 'Lorem ipsum ','\images\animals\squirel.png');
+values (1, 'Squirel',3, 20, 87, 9,9, 'Lorem ipsum ','\images\animals\squirel.jpg');
 insert into Cards (DeckId, [Name], Attr1, Attr2, Attr3, Attr4, Attr5, Flavourtext, Imagepath)
-values (1,'Sloth',34, 1,63, 42,3, 'Lorem ipsum ','\images\animals\sloth.png');
+values (1,'Sloth',34, 1,63, 42,3, 'Lorem ipsum ','\images\animals\sloth.jpg');
--- TopTrumps/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TopTrumps/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,7 @@ namespace TopTrumps.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/Home/Menu");
+            returnUrl ??= Url.Content("~/Menu/Index");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -103,7 +103,7 @@ namespace TopTrumps.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/Home/Menu");
+            returnUrl ??= Url.Content("~/Menu/Index");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
