888c5dc70366571da7ef5d180876e432899abc9b
Royabe <104497648+Royabe@users.noreply.github.com>
2022-06-27 16:05:47 +0100

    SQL purge

--- SQL/Insert_CardAttributes.sql
+++ SQL/Insert_CardAttributes.sql
@@ -1,6 +1,6 @@
-INSERT INTO CardAttributes(DeckId,attrname1,attrname2,attrname3,attrname4,attrname5)
+INSERT INTO Attribute(DeckId,attr1,attr2,attr3,attr4,attr5)
 values (0,'Year Since First Appearance','Number of Cards','Highest Mana Cost','Strength','Intelligence');
-INSERT INTO CardAttributes(DeckId,attrname1,attrname2,attrname3,attrname4,attrname5)
+INSERT INTO Attribute(DeckId,attr1,attr2,attr3,attr4,attr5)
 values (1,'Strength','Speed','Cuteness','Deadly','Intelligence');
-INSERT INTO CardAttributes(DeckId,attrname1,attrname2,attrname3,attrname4,attrname5)
+INSERT INTO Attribute(DeckId,attr1,attr2,attr3,attr4,attr5)
 values (2,'Bravery','Wisdom','Strength','Ferocity','Fear Factor');
