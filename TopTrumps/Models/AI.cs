9a9fcee880d9680cc618b54b4b9b3ff75a98c6fb
datasandwich <jarekettl98@gmail.com>
2022-06-27 12:34:19 +0200

    Same commit

---
+++ TopTrumps/Models/AI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TopTrumps.Models
+{
+    public class AI : Player
+    {
+        public bool Difficulty { get; set; }
+
+        public AI(string name, List<Deck> playerHand, bool difficulty) : base(name, playerHand)
+        {
+            Difficulty = difficulty;
+        }
+
+        public void play()
+        {
+            if (!IsActivePlayer)
+            {
+                //Check attribute selected by player
+            } else
+            {
+                string chosenAttribute = computeChoice();
+            }
+        }
+        public string computeChoice()
+        {
+            string computation = "";
+            return computation;
+        }
+    }
+}
+
---
+++ TopTrumps/Models/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace TopTrumps.Models
+{
+    public class Game
+    {
+
+        public Game()
+        {
+            
+        }
+        public void startGame()
+        {
+            /*
+            Deck activeDeck = new Deck(cards, owner, name);
+            Player playerOne = new Player("Roy", playerOneHand);
+            Player playerTwo = new AI("AI", playerTwoHand, difficulty);
+
+            playerOne.IsActivePlayer = true;
+
+            bool gameOver = false;
+
+            
+            while (!gameOver){
+            
+            Card playerOneTopCard = playerOne.dequeue(playerOneHand);
+            Card playerTwoTopCard = playerTwo.dequeue(playerTwoHand);
+
+            if (playerOne.IsActivePlayer){
+
+            } else {
+
+            }
+
+            if (playerOne.playerOneHand.Count == 0){
+                Console.WriteLine("Player Two Wins!");
+                gameOver = true;
+            } else {
+                Console.WriteLine("Player One Wins!"):
+                gameOver = true;
+            }
+            }
+
+            */
+        }
+        }
+    }
+
+
