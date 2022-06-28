8693d8df147584d9339a1dc586af876ae3563df1
Royabe <104497648+Royabe@users.noreply.github.com>
2022-06-28 13:24:42 +0100

    Fixed my BackImage

--- SQL/DeckSelectionData.sql
+++ SQL/DeckSelectionData.sql
@@ -1,6 +1,6 @@
 ﻿insert into Deck([Name],[ImagePath])
-values ('Magic the Gathering Planeswalkers','\TopTrumps\wwwroot\images\MTGDeck\BackImage.jpg');
+values ('Magic the Gathering Planeswalkers','\images\Planeswalkers\BackImage.jpg');
 insert into Deck([Name],[ImagePath])
-values ('Animals','\TopTrumps\wwwroot\images\Animals\BackImage.png');--Magda, replace this with the path of the image you use for the back
+values ('Animals','\images\Animals\BackImage.jpg');--Magda, replace this with the path of the image you use for the back
 insert into Deck([Name],[ImagePath])
-values ('Greek Mythology','\TopTrumps\wwwroot\images\GreekMyth\BackImage.jpg');--Jarek, replace this with the path of the image you use for the back
+values ('Greek Mythology','\images\GM\Aphrodite.jpeg');--Jarek, replace this with the path of the image you use for the back
