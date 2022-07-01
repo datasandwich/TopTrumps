41289a33bea7a7a2853456f7bd731ba5b814ae13
datasandwich <jarekettl98@gmail.com>
2022-07-01 16:10:09 +0200

    AI changes

--- TopTrumps/Models/AI.cs
+++ TopTrumps/Models/AI.cs
@@ -4,9 +4,9 @@ namespace TopTrumps.Models
 {
     public class AI : Player
     {
-        public bool Difficulty { get; set; }
+        public string Difficulty { get; set; }
 
-        public AI(string name, Deck playerHand, bool difficulty) : base(name, playerHand)
+        public AI(string name, Deck playerHand, string difficulty) : base(name, playerHand)
         {
             Difficulty = difficulty;
         }
@@ -14,29 +14,53 @@ namespace TopTrumps.Models
         
         public string choose(Card card, Attributes attributes)
         {
-
+            string choice = "";
+            /*
+            // Setting the max values for the attributes based on the deck
+            int[] attr_max = new int[5];
             switch (card.Deckid)
             {
                 case 0:
-                    int[] attr_max = { 0,0,0,0,0};
+                    attr_max[0] = 0;
+                    attr_max[1] = 0;
+                    attr_max[2] = 0;
+                    attr_max[3] = 0;
+                    attr_max[4] = 0;
                     break;
                 case 1:
-                    int[] attr_max = { 40, 100, 200, 15, 50 };
+                    attr_max[0] = 0;
+                    attr_max[1] = 0;
+                    attr_max[2] = 0;
+                    attr_max[3] = 0;
+                    attr_max[4] = 0;
                     break;
                 case 2:
-                    int[] attr_max = { 0, 0, 0, 0, 0 };
+                    attr_max[0] = 40;
+                    attr_max[1] = 110;
+                    attr_max[2] = 210;
+                    attr_max[3] = 20;
+                    attr_max[4] = 50;
                     break;
             }
-            string choice = "";
 
+            
+            int[] attributeValues = { card.Attr1, card.Attr2, card.Attr3, card.Attr4, card.Attr5 };
             float[] percentages = { card.Attr1 / attr_max[0], card.Attr2 / attr_max[1], card.Attr3 / attr_max[2], card.Attr4 / attr_max[3], card.Attr5 / attr_max[3] };
 
-            var maxIndex = percentages.IndexOf(percentages.Max());
+            // Select the attribute to choose based on the difficulty of the AI
+            int chosenIndex;
+            if (Difficulty == "easy")
+            {
+                chosenIndex = attributeValues.IndexOf(attributeValues.Max());
+            } else
+            {
+                chosenIndex = percentages.IndexOf(percentages.Max());
+            }
 
             string[] cardAttributes = { attributes.Attr1, attributes.Attr2, attributes.Attr3, attributes.Attr4, attributes.Attr5 };
+            choice = cardAttributes[chosenIndex];
 
-            choice = cardAttributes[maxIndex];
-
+            */
             return choice;
         }
     }
--- TopTrumps/Models/Game.cs
+++ TopTrumps/Models/Game.cs
@@ -27,11 +27,11 @@ namespace TopTrumps.Models
             }
             else if (mode == "EasyAI")
             {
-                player2 = new AI("TrumpNovice", new(0, "", ""), false);
+                player2 = new AI("TrumpNovice", new(0, "", ""), "easy");
             }
             else
             {
-                player2 = new AI("TrumpMaster", new(0, "", ""), true);
+                player2 = new AI("TrumpMaster", new(0, "", ""), "hard");
             }
             if (deck.Id != 0)
             {
